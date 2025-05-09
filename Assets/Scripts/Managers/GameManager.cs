66e977ba5e7547e9e962f44c5b9c8c2bf636d054
JuanRomo-dev <juanromo@ucm.es>
2025-05-09 22:21:22 +0200

    GameManager done (for now)

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -13,7 +13,7 @@ public enum GameState
 }
 public class GameManager : MonoBehaviour
 {
-    public static GameManager Instance;
+    public static GameManager Instance { get; private set; }
 
     public GameState currentGameState = GameState.Start;
 
@@ -21,7 +21,7 @@ public class GameManager : MonoBehaviour
     public float gameTime = 180f;   // 3 minutes in seconds
     public float reliability = 100f;
     public int popularity = 0;
-    private float timer;     // Timer 
+    private float _timer;     // Timer 
     
     [Header("Rounds")]
     public int currentRound = 0;
@@ -42,7 +42,7 @@ public class GameManager : MonoBehaviour
 
     void Start()
     {
-        timer = gameTime;
+        _timer = gameTime;
         LoadCaptcha();
     }
     
@@ -51,19 +51,50 @@ public class GameManager : MonoBehaviour
     {
         if (currentGameState == GameState.PlayingRound)
         {
-            timer -= Time.deltaTime;
-            if (timer <= 0)
+            _timer -= Time.deltaTime;
+            
+            if (_timer <= 0f)
             {
                 // End game
+                EndGame(true);
             }
         }
     }
 
     void LoadCaptcha()
     {
-        
+        currentGameState = GameState.Captcha;
     }
 
+    void OnCaptchaCompleted()
+    {
+        StartRound();
+    }
+
+    void StartRound()
+    {
+        currentGameState = GameState.PlayingRound;
+        currentRound++;
+    }
+
+    void SubmitRound()
+    {
+        currentGameState = GameState.RoundEnded;
+
+        int numberOfNews = 0;
+        int correctNews = 0;
+        int incorrectNews = 0;
+        
+        int reliability = correctNews + (incorrectNews * -3);
+
+        if (reliability < 50.0f)
+        {
+            EndGame(false);
+        } else {
+            // Start new round
+        }
+    }
+    
     void EndGame(bool win)
     {
         currentGameState = win ? GameState.GameWon : GameState.GameOver;
