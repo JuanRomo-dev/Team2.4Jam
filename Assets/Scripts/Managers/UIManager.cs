a40360a31c544201c71f89f5fd926538e4c155e3
Martin Perez <martinx902@gmail.com>
2025-05-10 07:08:17 +0200

    Credibility and followers moved to mobile on the left, clock UI and behaviour added to the game, yet to see how it works.

--- Assets/Scripts/Managers/UIManager.cs
+++ Assets/Scripts/Managers/UIManager.cs
@@ -1,21 +1,63 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 using TMPro;
 
 public class UIManager : MonoBehaviour
 {
+
+    [Header("PC VIEW ASSETS")]
+
     public GameObject newsPrefab;
     public GameObject prompList;
+    private List<UINewsBehaviour> actualNewsList;
+
+    [Header("ROOM VIEW ASSETS")]
 
     public TextMeshProUGUI followers;
     public TextMeshProUGUI credibility;
 
-    private List<UINewsBehaviour> actualNewsList;
+    public Image followersArrow;
+    public Image CredibilityArrow;
+
+    private float actualFollowers;
+    private float actualCredibility;
+
+    public Sprite arrowSUPER;
+    public Sprite arrowUP;
+    public Sprite arrowDOWN;
+
+    public TextMeshProUGUI hoursTxt;
+    public TextMeshProUGUI minutesTxt;
+    public float timeAccelerationRate = 4f;
+
+    private float gameTimeInMinutes = 8 * 60; // Start at 08:00 (8 hours * 60 mins)
+
+    private bool gameStarted = false;
 
     private void Awake()
     {
         actualNewsList = new List<UINewsBehaviour>();
+        credibility.text = "100";
+        followers.text = "0";
+    }
+
+    private void Update()
+    {
+        if (!gameStarted)
+            return;
+
+        // Advance game time
+        gameTimeInMinutes += Time.deltaTime * (timeAccelerationRate / 60f);
+
+        // Clamp to 24-hour format
+        int hours = ((int)gameTimeInMinutes / 60) % 24;
+        int minutes = ((int)gameTimeInMinutes % 60);
+
+        // Update UI
+        hoursTxt.text = hours.ToString("D2");
+        minutesTxt.text = minutes.ToString("D2");
     }
 
     //Instanciar por cada noticia de la lista de esta ronda una tarjeta y ponerla como hija de la promp list
@@ -31,6 +73,8 @@ public class UIManager : MonoBehaviour
 
             actualNewsList.Add(newPrompt);
         }
+
+        gameStarted = true;
     }
 
     public void EndRound(float newFollowers, float newCredibility)
@@ -51,10 +95,68 @@ public class UIManager : MonoBehaviour
     //Actualizar contadores de seguidores y credibilidad una vez acabes la ronda de noticias 
     private void UpdateCounters(float newFollowers, float newCredibility)
     {
-        followers.text = newFollowers.ToString();
-        credibility.text = newCredibility.ToString();
+        CheckCountersUpdate(newFollowers, newCredibility);
     }
 
+    private void CheckCountersUpdate(float newFollowers, float newCredibility)
+    {
+        if(actualFollowers == 0)
+        {
+            actualFollowers = newFollowers;
+            followersArrow.sprite = arrowUP;
+        }
+        else
+        {
+            float deltaFollowers = newFollowers - actualFollowers;
 
+            if (deltaFollowers >= 10)
+            {
+                //SUPER ARROW
+                followersArrow.sprite = arrowSUPER;
+            }
+            else if (deltaFollowers >= 5)
+            {
+                //UP ARROW
+                followersArrow.sprite = arrowUP;
+            }
+            else if (deltaFollowers <= 0)
+            {
+                //DOWN ARROW
+                followersArrow.sprite = arrowDOWN;
+            }
+        }
+
+        if (actualCredibility == 0)
+        {
+            actualCredibility = newCredibility;
+            CredibilityArrow.sprite = arrowUP;
+        }
+        else
+        {
+            float deltaCredibility = actualCredibility - newCredibility;
+
+            if (deltaCredibility >= 10)
+            {
+                //DOWN ARROW
+                CredibilityArrow.sprite = arrowDOWN;
+            }
+            else if (deltaCredibility >= 5)
+            {
+                //UP ARROW
+                CredibilityArrow.sprite = arrowUP;
+            }
+            else if (deltaCredibility <= 0)
+            {
+                //SUPER ARROW
+                CredibilityArrow.sprite = arrowSUPER;
+            }
+        }
+
+        actualFollowers = newFollowers;
+        actualCredibility = newCredibility;
+
+        followers.text = actualFollowers.ToString();
+        credibility.text = actualCredibility.ToString();
+    }
 
 }
