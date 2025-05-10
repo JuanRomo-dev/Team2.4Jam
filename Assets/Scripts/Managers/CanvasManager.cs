f634e186b032acfa0c4e3a8cc5cfba8ef72aa42e
Martin Perez <martinx902@gmail.com>
2025-05-10 04:00:59 +0200

    News Images added to the project

--- Assets/Scripts/Managers/CanvasManager.cs
+++ Assets/Scripts/Managers/CanvasManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class CanvasManager : MonoBehaviour
 {
@@ -13,6 +14,10 @@ public class CanvasManager : MonoBehaviour
         screenSpaceCanva.SetActive(false);
     }
 
-    
+    public void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Exiting game...");
+    }
 
 }
